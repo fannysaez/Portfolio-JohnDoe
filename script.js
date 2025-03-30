ac04e6c186f998c43973ace6a8d03fa5e98608a2
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-03-30 06:51:01 +0200

    add mise à jour script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let progress = 0;
 const progressBar = document.getElementById("progress-bar");
 const progressText = document.getElementById("progress-text");
 const loader = document.getElementById("loader");
+const typewriter = document.querySelector('.Typewriter');
 
 // Simule un chargement progressif avec une augmentation de 6% toutes les 300ms
 const loadingInterval = setInterval(() => {
@@ -18,10 +19,14 @@ const loadingInterval = setInterval(() => {
         clearInterval(loadingInterval);
         setTimeout(() => {
             loader.style.opacity = "0";
-            setTimeout(() => loader.style.display = "none", 500);
+            setTimeout(() => {
+                loader.style.display = "none"; // Cacher le loader après 0.5s
+                typewriter.classList.add('startTypewriter'); // Ajouter la classe après le loader
+            }, 500);
         }, 300);
     }
 }, 300); // Intervalle de 300ms
+
 
 
 
