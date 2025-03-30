83474cc383566ea1bc7099fcf7c660e36694559b
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-03-30 06:40:36 +0200

    add mise à jour script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 //Loader ! barre de progression animer
+// Initialisation des variables
 let progress = 0;
 const progressBar = document.getElementById("progress-bar");
 const progressText = document.getElementById("progress-text");
 const loader = document.getElementById("loader");
 
-// Simule un chargement progressif
+// Simule un chargement progressif avec une augmentation de 6% toutes les 300ms
 const loadingInterval = setInterval(() => {
-    progress += 5;
+    progress += 6;
     progressBar.style.width = progress + "%";
     progressText.innerText = progress + "%"; // Mise à jour du texte
 
     if (progress >= 100) {
+        progress = 100; // Pour éviter d'aller au-delà de 100%
+        progressBar.style.width = "100%";
+        progressText.innerText = "100%"; // Affiche "100%" à la fin du chargement
         clearInterval(loadingInterval);
         setTimeout(() => {
             loader.style.opacity = "0";
             setTimeout(() => loader.style.display = "none", 500);
         }, 300);
     }
-}, 100);
+}, 300); // Intervalle de 300ms
+
 
 
 // Effet de bascule (Toggle Effect) pour tous les services
